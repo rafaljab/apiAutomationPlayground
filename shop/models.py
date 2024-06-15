239df5f9ef2802f1e777b1acb8ff9d2ab685180d
Rafal Jablonski <jablonski.rafal@gmail.com>
2024-06-15 11:50:41 +0200

    feat: add Product model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+def product_path(instance: "Product", filename: str):
+    extension = filename.split(".")[-1]
+    name = instance.title.lower().replace(" ", "_")
+    new_filename = ".".join([name, extension])
+    return "/".join(["shop/products", new_filename])
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to=product_path)
+
+    def __str__(self):
+        return self.title
