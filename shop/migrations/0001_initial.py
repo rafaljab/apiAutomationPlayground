1c5a202c4e34c70a351889e306e97f06900c4bb9
Rafal Jablonski <jablonski.rafal@gmail.com>
2024-07-31 23:06:46 +0200

    style: reformat files

--- shop/migrations/0001_initial.py
+++ shop/migrations/0001_initial.py
@@ -8,18 +8,25 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Product',
+            name="Product",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=100)),
-                ('description', models.TextField()),
-                ('price', models.DecimalField(decimal_places=2, max_digits=5)),
-                ('image', models.ImageField(upload_to=shop.models.product_path)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=100)),
+                ("description", models.TextField()),
+                ("price", models.DecimalField(decimal_places=2, max_digits=5)),
+                ("image", models.ImageField(upload_to=shop.models.product_path)),
             ],
         ),
     ]
